593db0061d65b709f687dce86b15e787b2232188
NunesNathan <nunesxd7@hotmail.com>
2022-09-04 17:31:47 -0300

    feat: study_schedule works

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
+from types import NoneType
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    if isinstance(target_time, NoneType):
+        return None
+    logons = int()
+    for (login, logout) in permanence_period:
+        if not isinstance(login, int) or not isinstance(logout, int):
+            return None
+        if target_time in range(login, logout + 1):
+            logons += 1
+
+    return logons
