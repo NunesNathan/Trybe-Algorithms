98b17a8260d8ba3c1788d620911a7d7372a2f4df
NunesNathan <nunesxd7@hotmail.com>
2022-09-04 17:48:08 -0300

    feat: recursive anagram check works

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,14 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    if len(list1) != len(list2):
+        return False
+
+    list1 = list(first_string.lower())
+    list2 = list(second_string.lower())
+
+    for i in list1:
+        if i in list2:
+            list2.remove(i)
+        else:
+            return False
+
+    return True
